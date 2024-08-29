8919acfe0c1c8e8de3fe92a2ddf9fb9e7a066ed4
t6adev <t.yamamoto28@gmail.com>
2024-08-29 21:20:03 +0900

    update waku@0.21.0

--- src/kv.ts
+++ src/kv.ts
@@ -1,7 +1,7 @@
-import { getContext } from 'waku/server';
+import { unstable_getCustomContext as getCustomContext } from 'waku/server';
 
 export const getKV = async (key: string) => {
-  const context = getContext<{ bindings: Bindings }>();
+  const context = getCustomContext<{ bindings: Bindings }>();
   const val = await context.bindings?.WAKU_SAMPLE?.get(key);
   return val;
 };
